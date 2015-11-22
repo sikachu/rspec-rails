4e0c865929a3f699a13956f12c13d64557ce1e4a
Sam Phippen <samphippen@googlemail.com>
2015-11-22 14:06:32 +0000

    Clear and merge test response headers instead of assigning.
    Rails 5 removes the attr_writer for this method, but clearing the hash
    and writing to it is essentially equivalent to assigning it.

--- lib/rspec/rails/matchers/have_http_status.rb
+++ lib/rspec/rails/matchers/have_http_status.rb
@@ -43,7 +43,8 @@ module RSpec
             # Capybara or catch `NameError`s for the undefined constants
             obj = ActionDispatch::Response.new.tap do |resp|
               resp.status  = obj.status_code
-              resp.headers = obj.response_headers
+              resp.headers.clear
+              resp.headers.merge!(obj.response_headers)
               resp.body    = obj.body
             end
             ::ActionDispatch::TestResponse.from_response(obj)
